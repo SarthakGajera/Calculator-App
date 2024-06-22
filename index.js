3e57964d1c56fd8a0a317cdc3a39b5f5c5a683be
SarthakGajera <151034057+SarthakGajera@users.noreply.github.com>
2024-06-21 22:43:39 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const input=document.getElementById('inputbox');
+const buttons=document.querySelectorAll('button');
+let string="";
+let arr=Array.from(buttons)
+arr.forEach(button=>{
+    button.addEventListener('click',(e)=>{
+        const value=e.target.innerHTML;
+        if(value==="="){
+            string=eval(string);
+            input.value=string
+        }
+        else if(value=="AC"){
+            string="";
+            input.value=string;
+            
+        }
+        else if(value=="DEL"){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        }
+        else{
+        string+=value;
+        input.value=string;
+
+        }
+        
+    })
+})
